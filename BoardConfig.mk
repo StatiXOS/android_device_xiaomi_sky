934255c72ad3f3918f986cd28fb235638832a8a2
Kuba Wojciechowski <nullbytepl@gmail.com>
2023-03-07 06:55:10 +0000

    sky: Disable kpti
    Change-Id: I1f8b25b7445664ee10a9dbd7f111becabe8111c4
    Signed-off-by: Vaisakh Murali <mvaisakh@statixos.com>

--- BoardConfig.mk
+++ BoardConfig.mk
@@ -83,7 +83,8 @@ BOARD_KERNEL_CMDLINE := video=vfb:640x400,bpp=32,memsize=3072000
 BOARD_BOOTCONFIG := \
     androidboot.hardware=qcom \
     androidboot.memcg=1 \
-    androidboot.usbcontroller=a600000.dwc3
+    androidboot.usbcontroller=a600000.dwc3 \
+    kpti=off
 
 BOARD_INCLUDE_DTB_IN_BOOTIMG := true
 BOARD_BOOT_HEADER_VERSION := 4

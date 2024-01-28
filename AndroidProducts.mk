389618d5cb6ac7cd81a41479d98af55c18b2c740
Vaisakh Murali <mvaisakh@statixos.com>
2024-01-28 22:40:43 +0530

    sky: initial statix makefiles
    Change-Id: Ie6bd70cff446da0cb0186170b7f6991174063435

---
+++ AndroidProducts.mk
@@ -0,0 +1,10 @@
+#
+# Copyright 2014 The Android Open-Source Project
+# Copyright 2024 StatiXOS
+#
+# SPDX-License-Identifier: Apache-2.0
+#
+
+PRODUCT_MAKEFILES := $(LOCAL_DIR)/statix_sky.mk
+
+COMMON_LUNCH_CHOICES := statix_sky-user
---
+++ statix_sky.mk
@@ -0,0 +1,25 @@
+#
+# Copyright 2014 The Android Open-Source Project
+# Copyright 2024 StatiXOS
+#
+# SPDX-License-Identifier: Apache-2.0
+#
+
+# Inherit from those products. Most specific first.
+$(call inherit-product, $(SRC_TARGET_DIR)/product/core_64_bit.mk)
+$(call inherit-product, $(SRC_TARGET_DIR)/product/full_base_telephony.mk)
+
+# Inherit device specific configuration
+$(call inherit-product, device/xiaomi/sky/device.mk)
+
+# Inherit some common StatiX stuff.
+$(call inherit-product, vendor/statix/config/common.mk)
+$(call inherit-product, vendor/statix/config/gsm.mk)
+
+PRODUCT_NAME := statix_sky
+PRODUCT_DEVICE := sky
+PRODUCT_BRAND := Xiaomi
+PRODUCT_MODEL := sky
+PRODUCT_MANUFACTURER := Xiaomi
+
+PRODUCT_GMS_CLIENTID_BASE := android-xiaomi

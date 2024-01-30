0774b20f78a3607e6dab1b4a480bbaf6ade287b6
Sourajit Karmakar <sourajit.karmakar@gmail.com>
2024-01-30 12:36:52 +0530

    sky: import qcom config.fs
    From "LA.VENDOR.1.0.c2-01400-WAIPIO.0"
    
    Change-Id: I31d98c66a80da52f57e3678fc087ce59733171dd

--- BoardConfig.mk
+++ BoardConfig.mk
@@ -5,9 +5,12 @@
 # SPDX-License-Identifier: Apache-2.0
 #
 
+DEVICE_PATH := device/xiaomi/sky
+
 # Use the non-open-source parts, if they're present
 -include vendor/xiaomi/sky/BoardConfigVendor.mk
 
+# Architecture
 TARGET_ARCH := arm64
 TARGET_ARCH_VARIANT := armv8-a
 TARGET_CPU_ABI := arm64-v8a
@@ -17,3 +20,6 @@ TARGET_2ND_ARCH := arm
 TARGET_2ND_ARCH_VARIANT := armv8-2a
 TARGET_2ND_CPU_ABI := armeabi-v7a
 TARGET_2ND_CPU_ABI2 := armeabi
+
+# ConfigFS
+TARGET_FS_CONFIG_GEN := $(DEVICE_PATH)/configs/config.fs

4d563a9c9aa4c11072646490e07549f126dcddcf
Vaisakh Murali <mvaisakh@statixos.com>
2024-01-31 09:53:12 +0530

    sky: initial makefiles
    Generated using https://github.com/mvaisakh/aosp-device-tools/blob/udc-qpr1/populate-new-device.sh
    
    Change-Id: I24d1e8622cf6d71b8f12dc2280d9c2a91e195b83

---
+++ BoardConfig.mk
@@ -0,0 +1,19 @@
+#
+# Copyright 2014 The Android Open-Source Project
+# Copyright 2024 StatiXOS
+#
+# SPDX-License-Identifier: Apache-2.0
+#
+
+# Use the non-open-source parts, if they're present
+-include vendor/xiaomi/sky/BoardConfigVendor.mk
+
+TARGET_ARCH := arm64
+TARGET_ARCH_VARIANT := armv8-a
+TARGET_CPU_ABI := arm64-v8a
+TARGET_CPU_ABI2 :=
+
+TARGET_2ND_ARCH := arm
+TARGET_2ND_ARCH_VARIANT := armv8-2a
+TARGET_2ND_CPU_ABI := armeabi-v7a
+TARGET_2ND_CPU_ABI2 := armeabi

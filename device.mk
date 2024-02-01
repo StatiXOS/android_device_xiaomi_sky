f1e4d1af462d590f3e5f8c4f26d545384b1c8b92
Vaisakh Murali <mvaisakh@statixos.com>
2024-02-01 16:06:28 +0530

    sky: build soundtrigger 2.3 impl
    Change-Id: I47832e5d0071022e426832b4870051782a942cad

--- device.mk
+++ device.mk
@@ -40,7 +40,8 @@ PRODUCT_PACKAGES += \
 PRODUCT_PACKAGES += \
     android.hardware.audio.service \
     android.hardware.audio@7.0-impl \
-    android.hardware.audio.effect@7.0-impl
+    android.hardware.audio.effect@7.0-impl \
+    android.hardware.soundtrigger@2.3-impl
 
 # Fastbootd
 PRODUCT_PACKAGES += \

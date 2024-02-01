67984a3db20c67c67391baf94e94db23170fd5e8
Vaisakh Murali <mvaisakh@statixos.com>
2024-02-01 20:03:25 +0530

    sky: import HIDL vintf matrices
    From clo board, qssi and common trees.
    
    Change-Id: Iecc4094b2cfb9c8b6e3f07f7cd794f73a4f8759c

--- BoardConfig.mk
+++ BoardConfig.mk
@@ -27,6 +27,16 @@ BUILD_BROKEN_ELF_PREBUILT_PRODUCT_COPY_FILES := true
 # ConfigFS
 TARGET_FS_CONFIG_GEN := $(DEVICE_PATH)/configs/config.fs
 
+# HIDL Vintf
+DEVICE_FRAMEWORK_COMPATIBILITY_MATRIX_FILE += \
+    $(DEVICE_PATH)/configs/vintf/compatibility_matrix.xml \
+
+DEVICE_MANIFEST_FILE := \
+    $(DEVICE_PATH)/configs/vintf/manifest.xml
+
+DEVICE_MATRIX_FILE := \
+    $(DEVICE_PATH)/configs/vintf/compatibility_matrix.xml
+
 # Kernel
 BOARD_KERNEL_BASE := 0x00000000
 BOARD_KERNEL_PAGESIZE := 4096

bdee85ed9b74840d1fbd044b744142b87e1162c7
Anay Wadhera <awadhera@berkeley.edu>
2024-02-03 23:51:28 -0500

    sky: Patch keymint to include rsk aidl for missing symbol
    Change-Id: Ic6165443aad515d5fb5852f94a656678b534bac9

--- extract-files.sh
+++ extract-files.sh
@@ -54,6 +54,14 @@ if [ -z "${SRC}" ]; then
     SRC="adb"
 fi
 
+function blob_fixup() {
+    case "${1}" in
+        vendor/bin/hw/android.hardware.security.keymint-service-qti)
+            "${PATCHELF}" --add-needed android.hardware.security.rkp-V1-ndk_platform.so "${2}"
+            ;;
+    esac
+}
+
 # Initialize the helper
 setup_vendor "${DEVICE}" "${VENDOR}" "${ANDROID_ROOT}" false "${CLEAN_VENDOR}"
 

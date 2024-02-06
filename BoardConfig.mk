9b63efdcf0849e5bb6da5536088fb8ee48ec9ba0
Anay Wadhera <anay1018@gmail.com>
2024-02-05 22:55:53 -0500

    sky: Add framework matrix and cleanup vintf
    Change-Id: Iddfb2ca19c71b3d7591dae5fcb320df2f7f55e79

--- BoardConfig.mk
+++ BoardConfig.mk
@@ -54,12 +54,15 @@ TARGET_SCREEN_DENSITY := 440
 
 # HIDL Vintf
 DEVICE_FRAMEWORK_COMPATIBILITY_MATRIX_FILE += \
-    $(DEVICE_PATH)/configs/vintf/compatibility_matrix.xml \
+    $(DEVICE_PATH)/configs/vintf/framework_matrix.xml
 
-DEVICE_MANIFEST_FILE := \
+DEVICE_FRAMEWORK_MANIFEST_FILE += \
+    $(DEVICE_PATH)/configs/vintf/framework_manifest.xml \
+
+DEVICE_MANIFEST_FILE += \
     $(DEVICE_PATH)/configs/vintf/manifest.xml
 
-DEVICE_MATRIX_FILE := \
+DEVICE_MATRIX_FILE += \
     $(DEVICE_PATH)/configs/vintf/compatibility_matrix.xml
 
 # Kernel

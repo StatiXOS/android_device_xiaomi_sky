cd484decb233df814e1e40de04153ef62ff06bae
Vaisakh Murali <mvaisakh@statixos.com>
2024-02-07 23:24:01 +0530

    sky: build vendor variant of drm interface
    Change-Id: I51c48d68a15d8adf329cde06becce76121b84423

--- device.mk
+++ device.mk
@@ -89,7 +89,8 @@ PRODUCT_PACKAGES += \
 
 # DRM
 PRODUCT_PACKAGES += \
-    android.hardware.drm-service.clearkey
+    android.hardware.drm-service.clearkey \
+    android.hardware.drm@1.4.vendor
 
 # Data
 PRODUCT_PACKAGES += \

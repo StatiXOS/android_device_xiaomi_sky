cfa72659ad220a990562ae503c156e8bd99c9a39
Anay Wadhera <awadhera@berkeley.edu>
2024-02-07 23:29:29 +0530

    sky: build vendor variant of libqti_vndfwk_detect
    Change-Id: Ida73c7e39bc62b329bbcf43e17584eed033bdaaa

--- device.mk
+++ device.mk
@@ -313,6 +313,7 @@ PRODUCT_COPY_FILES += \
     prebuilts/vndk/v32/arm64/arch-arm64-armv8-a/shared/vndk-sp/libhidlbase.so:$(TARGET_COPY_OUT_VENDOR)/lib64/libhidlbase-v32.so
 
 PRODUCT_PACKAGES += \
+    libqti_vndfwk_detect.vendor \
     libqti_vndfwk_detect_vendor
 
 # WiFi

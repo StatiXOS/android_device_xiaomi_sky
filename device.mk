a1028de6c88a4c65aaa1a59a210e8f3cdd9f0dfd
Anay Wadhera <awadhera@berkeley.edu>
2024-02-07 23:25:15 +0530

    sky: build vendor variant of GNSS interface
    Change-Id: I0a317814599a41dbce35303899edfb0f6fc576e2

--- device.mk
+++ device.mk
@@ -108,6 +108,10 @@ PRODUCT_PACKAGES += \
 PRODUCT_COPY_FILES += \
     frameworks/native/data/etc/android.hardware.fingerprint.xml:$(TARGET_COPY_OUT_VENDOR)/etc/permissions/android.hardware.fingerprint.xml
 
+# GNSS
+PRODUCT_PACKAGES += \
+    android.hardware.gnss@2.1.vendor
+
 # Gatekeeper
 PRODUCT_PACKAGES += \
     android.hardware.gatekeeper@1.0.vendor

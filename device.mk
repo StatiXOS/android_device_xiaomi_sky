49dd4121fd1a61f3cbe7eecc2c8e270225ed8276
Vaisakh Murali <mvaisakh@statixos.com>
2024-02-08 12:58:41 +0530

    sky: setup usb feature configs
    Change-Id: I0f95e49229c2c5620513a0a577f11ff2c0fbd01e

--- device.mk
+++ device.mk
@@ -328,6 +328,10 @@ PRODUCT_PACKAGES += \
 # USB
 $(call inherit-product, vendor/qcom/opensource/usb/vendor_product.mk)
 
+PRODUCT_COPY_FILES += \
+    frameworks/native/data/etc/android.hardware.usb.accessory.xml:$(TARGET_COPY_OUT_VENDOR)/etc/permissions/android.hardware.usb.accessory.xml \
+    frameworks/native/data/etc/android.hardware.usb.host.xml:$(TARGET_COPY_OUT_VENDOR)/etc/permissions/android.hardware.usb.host.xml
+
 # Vendor Service Manager
 PRODUCT_PACKAGES += \
     vndservicemanager

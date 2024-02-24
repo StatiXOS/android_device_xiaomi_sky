d764edfc9cef0e969fafe007add592e46b81bb62
Vaisakh Murali <mvaisakh@statixos.com>
2024-02-24 21:18:29 +0530

    sky: build consumerir default hal module
    Change-Id: If9bda0763c009421e6b77c22b654c11ebacdc98e

--- device.mk
+++ device.mk
@@ -173,7 +173,8 @@ PRODUCT_COPY_FILES += \
 
 PRODUCT_PACKAGES += \
     android.hardware.ir@1.0-impl \
-    android.hardware.ir@1.0-service
+    android.hardware.ir@1.0-service \
+    consumerir.default
 
 # Init
 PRODUCT_COPY_FILES += \

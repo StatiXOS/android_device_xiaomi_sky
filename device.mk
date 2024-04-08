a07768b89d80f56bbe1e872818f3de040ebc0da9
Vaisakh Murali <vaisakhmurali@gmail.com>
2024-04-08 10:00:51 +0530

    sky: support pro and low latency audio
    Change-Id: I2ecc2d618f7e918382c7b02aa44939d81fdad992

--- device.mk
+++ device.mk
@@ -64,7 +64,9 @@ PRODUCT_PACKAGES += \
     vendor.qti.hardware.pal@1.0.vendor
 
 PRODUCT_COPY_FILES += \
-    frameworks/native/data/etc/android.software.midi.xml:$(TARGET_COPY_OUT_VENDOR)/etc/permissions/android.software.midi.xml
+    frameworks/native/data/etc/android.software.midi.xml:$(TARGET_COPY_OUT_VENDOR)/etc/permissions/android.software.midi.xml \
+    frameworks/native/data/etc/android.hardware.audio.pro.xml:$(TARGET_COPY_OUT_VENDOR)/etc/permissions/android.hardware.audio.pro.xml \
+    frameworks/native/data/etc/android.hardware.audio.low_latency.xml:$(TARGET_COPY_OUT_VENDOR)/etc/permissions/android.hardware.audio.low_latency.xml
 
 
 # Bluetooth

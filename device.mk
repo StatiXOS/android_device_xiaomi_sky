5675b0da28bdcf3a1e92d2a56e55cd7cb878fc53
Vaisakh Murali <vaisakhmurali@gmail.com>
2024-04-08 18:04:37 +0530

    sky: build OSS IFAA
    Change-Id: I0658a9d254c876ca87603ed221cf7db7d4ca1deb

--- device.mk
+++ device.mk
@@ -277,6 +277,10 @@ PRODUCT_PACKAGES += \
 # Partitions
 PRODUCT_USE_DYNAMIC_PARTITIONS := true
 
+# Payments
+PRODUCT_PACKAGES += \
+    IFAAService
+
 # Perf
 PRODUCT_PACKAGES += \
     vendor.qti.hardware.perf@2.3.vendor

b5a5963eb0f6a14febd3523250c8007c9e39b316
Vaisakh Murali <vaisakhmurali@gmail.com>
2024-06-18 22:48:00 +0530

    sky: Switch to OSS libperfmgr
    Entirely re-written powerhint.
    
    Change-Id: Ib4dfdb7fafd0e0bab6591fe516a3d87e42a06ffe

--- device.mk
+++ device.mk
@@ -324,8 +324,12 @@ PRODUCT_PACKAGES += \
     vendor.qti.hardware.perf@2.3.vendor
 
 # Power
-$(call inherit-product, vendor/qcom/opensource/power/power-vendor-board.mk)
-$(call inherit-product, vendor/qcom/opensource/power/power-vendor-product.mk)
+PRODUCT_COPY_FILES += \
+    $(LOCAL_PATH)/configs/powerhint.json:$(TARGET_COPY_OUT_VENDOR)/etc/powerhint.json
+
+PRODUCT_PACKAGES += \
+    android.hardware.power-service.statix-libperfmgr \
+    libqti-perfd-client
 
 # Project ID Quota
 $(call inherit-product, $(SRC_TARGET_DIR)/product/emulated_storage.mk)
@@ -435,6 +439,7 @@ PRODUCT_SHIPPING_API_LEVEL := 33
 # Soong
 PRODUCT_SOONG_NAMESPACES += \
     $(LOCAL_PATH) \
+    hardware/statix/interfaces/power-libperfmgr \
     vendor/hardware/xiaomi
 
 # Symlinks

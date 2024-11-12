2c77886038e840b4adcbbc0cd7e2cb10001e5a19
Sourajit Karmakar <sourajit.karmakar@gmail.com>
2024-11-12 09:17:23 -0500

    sky: Move away from split makefile for USB HAL
    Change-Id: I3d5103419da6b2806c4c5386bc615e70b6218d4a

--- device.mk
+++ device.mk
@@ -602,8 +602,17 @@ PRODUCT_PACKAGES += \
     android.hardware.thermal@2.0.vendor
 
 # USB
+PRODUCT_PACKAGES += \
+    android.hardware.usb-service.qti \
+    android.hardware.usb.gadget-service.qti
+
+PRODUCT_PACKAGES += \
+    init.qcom.usb.rc \
+    init.qcom.usb.sh
+
+PRODUCT_SOONG_NAMESPACES += vendor/qcom/opensource/usb/etc
+
 TARGET_HAS_DIAG_ROUTER := true
-$(call inherit-product, vendor/qcom/opensource/usb/vendor_product.mk)
 
 PRODUCT_COPY_FILES += \
     frameworks/native/data/etc/android.hardware.usb.accessory.xml:$(TARGET_COPY_OUT_VENDOR)/etc/permissions/android.hardware.usb.accessory.xml \
